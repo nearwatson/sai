23e54a273af28837718c53e5da0be7116475689f
xuxsxs <firefox2911@aliyun.com>
2020-06-16 10:00:05 +0800

    app modified

--- app.py
+++ app.py
@@ -1,46 +1,23 @@
 from flask import Flask, request
+from flask import jsonify
+import json
 
+import os
 #实例化flask对象
 app = Flask(__name__)
 
-
-
 # status_model = 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-@app.route('/indexabc', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    inputs = request.form['text']
+    inputs = request.get_json()
+
+    print(json.dumps(inputs))
+    inputs = json.dumps(inputs)
+    # print(type(inputs), inputs, jsonify(inputs), type(jsonify(inputs)))
     # status = status_model(inputs)
     # return {'key': request.form['text']}
-    return {'key': request.form['text']}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return inputs # jsonify(inputs)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=int(os.getenv('PORT', 5000)))
